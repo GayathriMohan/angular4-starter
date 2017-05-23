c99c3bcfa046428648364112e19a0d5394b80748
Gayathri Mohan <gayathrimohan@qburst.com>
2017-05-23 17:06:21 +0530

    register

--- ang-starter-app/src/app/register/register.component.ts
+++ ang-starter-app/src/app/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../models/users';
+
 declare var $: any;
 
 @Component({
@@ -8,9 +10,21 @@ declare var $: any;
 })
 export class RegisterComponent implements OnInit {
 
+  persons: User[] = [];
+  counter = 0;
+  person: User;
+
   constructor() { }
+  onSubmit(value) {
+    this.person = new User(value.id, value.username, value.password, value.firstname, value.lastname);
+    if (value) {
+      this.persons.push(this.person);
+    }
+    this.counter++;
+  }
 
   ngOnInit() {
+    const showHide = false;
      $('.wrapper input').focus(function(){
          $(this).siblings().addClass('is-active');
       });
